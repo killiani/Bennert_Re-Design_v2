ec4e16344fc8d579c080ba0057ea1c42ad4bf30d
Ben <killiani@yahoo.de>
2021-06-24 11:14:24 +0200

    first push

---
+++ templates/menu.php
@@ -0,0 +1,13 @@
+
+
+<div class="menu-close">
+	<a data-mobile-menu="close" href="#">×</a></div>
+
+<div class="desktop">
+    {{insert_module::8}}
+</div>
+<div class="mobile">
+    {{insert_module::8}}
+</div>
+
+
