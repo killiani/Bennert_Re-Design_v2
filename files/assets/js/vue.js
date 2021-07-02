6b1cb0d3044a5497a2691e583bbb15ba07cf7a59
Ben <killiani@yahoo.de>
2021-07-02 16:43:24 +0200

    kleine Anpassungen im Sytle

--- files/assets/js/vue.js
+++ files/assets/js/vue.js
@@ -1,5 +1,5 @@
-const bio = new Vue({
-    el: '#menu-button',
+const desktop = new Vue({
+    el: '#menu-button-desktop',
     delimiters: ['${', '}'],
     data: {
         checked: false,
@@ -25,6 +25,8 @@ const bio = new Vue({
                 document.documentElement.scrollTop = 0;
             }
 
+
+
             function stopScrolling(){
                 let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
                 let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
@@ -32,6 +34,51 @@ const bio = new Vue({
                     window.onscroll = function() {
                         window.scrollTo(scrollLeft, scrollTop);
                     };
+
+            }
+            function enableScrolling(){
+                window.onscroll = function() {};
+            }
+        },
+    }
+});
+
+const mobile = new Vue({
+    el: '#menu-button-mobile',
+    delimiters: ['${', '}'],
+    data: {
+        checked: false,
+    },
+
+    methods: {
+        switchButton() {
+            let c = this.checked;
+
+            if (c) {
+                console.log('on');
+                this.checked = false;
+                enableScrolling();
+            } else {
+                console.log('off');
+                this.checked = true;
+                totop();
+                stopScrolling();
+            }
+
+            function totop(){
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
+            }
+
+
+
+            function stopScrolling(){
+                let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+                window.onscroll = function() {
+                    window.scrollTo(scrollLeft, scrollTop);
+                };
 
             }
             function enableScrolling(){
